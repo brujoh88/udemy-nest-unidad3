387b07778ec39f8badab817f16061a34270a1e26
Gus - MVC <gustavotiseira@gmail>
2025-08-27 10:53:22 -0300

    35

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+  private cars = ['Toyota', 'Ford', 'Honda', 'Reanult'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Ford', 'Honda', 'Reanult'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById( @Param('id') id : string ) {
+    return this.cars[id];
   }
 }
